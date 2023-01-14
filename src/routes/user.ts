62c55de5890c74e3c4e667f495f10ec4c0c01124
Ali Azlan Aziz <aliazlan2002@gmail.com>
2023-01-14 21:07:36 +0500

    routing files

---
+++ src/routes/user.ts
@@ -0,0 +1,17 @@
+import express, { Router } from "express";
+import {
+  checkReachable,
+  postSignup,
+  postSignin,
+} from "../controllers/student";
+import { isUserAuthenticated } from "../middlewares/isRoleAuthenticated";
+
+const router: Router = express.Router();
+
+router.get("/", checkReachable);
+
+router.post("/signup", postSignup);
+
+router.post("/signin", postSignin);
+
+export default router;
