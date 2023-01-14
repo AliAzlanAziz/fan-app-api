675b4af2db761aedab7b63117b5188e1999cc6b1
Ali Azlan Aziz <aliazlan2002@gmail.com>
2023-01-14 21:08:34 +0500

    controller functions

---
+++ src/controllers/student.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import {
+  Signin,
+  Signup,
+} from "../services/student";
+
+export const checkReachable = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  return res.status(200).json({ message: "User APIs reachabled" });
+};
+
+export const postSignup = (req: Request, res: Response, next: NextFunction) => {
+  return Signup(req.body.user, res);
+};
+
+export const postSignin = (req: Request, res: Response, next: NextFunction) => {
+  return Signin(req.body.user, res);
+};
